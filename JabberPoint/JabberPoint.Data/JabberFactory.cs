5b0cbcc159be8437e828e5f7911875efffc99b89
SVB <trialnterrorgames@gmail.com>
2019-10-02 20:40:11 +0200

    Added deserialization code for the xmlLoader class.

---
+++ JabberPoint/JabberPoint.Data/JabberFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JabberPoint.Data
+{
+    class JabberFactory
+    {
+        public JabberFactory()
+        {
+        }
+    }
+}
--- JabberPoint/JabberPoint.Data/XmlLoader.cs
+++ JabberPoint/JabberPoint.Data/XmlLoader.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using System.IO;
 using System.Xml;
+using System.Xml.Serialization;
 
 namespace JabberPoint.Data
 {
     public class XmlLoader
     {
+        private String InputUrl { get; set; }
+        public slideshow RootObject { get; private set; }
         public XmlLoader(string inputUrl)
         {
-
+            this.InputUrl = inputUrl;
         }
 
-        private void Read() { 
+        private void Read() {
+            XmlSerializer mySerializer = new XmlSerializer(typeof(slideshow));
+            this.RootObject = (slideshow)mySerializer.Deserialize(new FileStream(this.InputUrl, FileMode.Open));
         }
     }
 }

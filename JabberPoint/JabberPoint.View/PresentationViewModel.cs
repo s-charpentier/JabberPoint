b92b292e1a45bb127ce0273a46c2f1f0241409cb
Sam Van Battel <soms.goeiemorgen@gmail.com>
2019-10-05 12:25:22 +0200

    ui update:  - changed icons to paths  - removed shadow effect

--- JabberPoint/JabberPoint.View/PresentationViewModel.cs
+++ JabberPoint/JabberPoint.View/PresentationViewModel.cs
@@ -10,14 +10,14 @@ using System.Windows;
 
 namespace JabberPoint.View
 {
-    public abstract class ViewModel :INotifyPropertyChanged
+    public abstract class ViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName=null)
             =>PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));        
     }
-    public class PresentationViewModel:ViewModel
+    public class PresentationViewModel : ViewModel
     {
        
     }

b83e19f17bc7ba9e888f885e06417f65460c5fbf
s-charpentier <55657473+s-charpentier@users.noreply.github.com>
2019-10-08 12:59:52 +0200

    Update MediaBehaviour.cs

--- JabberPoint/JabberPoint.Domain/Content/Behaviours/MediaBehaviour.cs
+++ JabberPoint/JabberPoint.Domain/Content/Behaviours/MediaBehaviour.cs
@@ -14,6 +14,6 @@ namespace JabberPoint.Domain.Content.Behaviours
         public IContent Parent { get; set; }
         public string Reference { get; set; }
         public string IsPlaying { get; private set; }
-        public object Draw() => throw new InvalidOperationException("please only use view specific classes");
+        public object Draw(int pageNr) => throw new InvalidOperationException("please only use view specific classes");
     }
 }
